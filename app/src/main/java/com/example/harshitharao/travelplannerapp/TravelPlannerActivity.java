ff420d85375257608c345e19cdc9b0ac354c51d6
Harshitha A Rao <hrao20@gmail.com>
2018-07-04 18:07:42 +0530

    Add capture image facility and show the image in the main activity

--- app/src/main/java/com/example/harshitharao/travelplannerapp/TravelPlannerActivity.java
+++ app/src/main/java/com/example/harshitharao/travelplannerapp/TravelPlannerActivity.java
@@ -1,15 +1,19 @@
 package com.example.harshitharao.travelplannerapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class TravelPlannerActivity extends AppCompatActivity {
-    private int TIME_ACTIVITY_CODE = 1;
+    private static final int TIME_ACTIVITY_CODE = 1;
+    private static final int CAMERA_ACTIVITY_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +42,16 @@ public class TravelPlannerActivity extends AppCompatActivity {
                 totalTimeView.setText(totalTime);
             }
         }
+        if (requestCode == CAMERA_ACTIVITY_CODE) {
+            if (resultCode == RESULT_OK) {
+                ImageView imageView = findViewById(R.id.image);
+                imageView.setImageBitmap((Bitmap) data.getExtras().get("data"));
+            }
+        }
+    }
+
+    public void capture(View view) {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_ACTIVITY_CODE);
     }
 }
